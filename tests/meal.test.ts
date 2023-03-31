277f8af73b5b3d6bbc226d6bb587eb5a461497b4
Starcea <stardev.uwu@gmail.com>
2023-03-31 22:19:45 +0900

    feat: better tests

---
+++ tests/meal.test.ts
@@ -0,0 +1,34 @@
+import { Neis } from '../src'
+import { School } from '../src/structures/school'
+import { MealInfo } from '../src/types'
+import { Logger } from 'tslog'
+
+const logger = new Logger()
+
+const neis = new Neis({
+  logger: logger,
+})
+
+describe('Meal', () => {
+  it('should return MealInfo[]', async () => {
+    const schools = (await neis.getSchool('B10')) as School[]
+
+    const data: MealInfo[] = []
+    for await (const school of schools) {
+      data.push((await school.getMeal('20230331')) as MealInfo)
+    }
+
+    logger.info(data)
+
+    expect(data).toMatchObject<MealInfo[]>(data)
+  })
+
+  it('should return MealServiceDietInfo', async () => {
+    const school = (await neis.getSchool('B10', '7091455')) as School
+
+    const data = (await school.getMeal('20230331')) as MealInfo
+    logger.info(data)
+
+    expect(data).toMatchObject<MealInfo>(data)
+  })
+})
--- tests/school.test.ts
+++ tests/school.test.ts
@@ -3,19 +3,20 @@ import { School } from '../src/structures/school'
 import { Logger } from 'tslog'
 
 const logger = new Logger()
+
 const neis = new Neis({
   logger: logger,
 })
 
-describe('schoolInfo', () => {
-  it('should return SchoolInfo[]', async () => {
+describe('School', () => {
+  it('should return School[]', async () => {
     const data = (await neis.getSchool('B10')) as School[]
     logger.info(data)
 
     expect(data).toMatchObject<School[]>(data)
   })
 
-  it('should return SchoolInfo', async () => {
+  it('should return School', async () => {
     const data = (await neis.getSchool('B10', '7091455')) as School
     logger.info(data)
 
