27a5518e730507fbcc5370b3d89b4a06c4af8d2e
Starcea <stardev.uwu@gmail.com>
2023-04-06 19:39:19 +0900

    feat: meal info tests

--- tests/meal.test.ts
+++ tests/meal.test.ts
@@ -29,4 +29,33 @@ describe('Meal', () => {
 
     expect(data).toMatchObject<Meal>(data)
   })
+
+  it('should return meal infos', async () => {
+    const school = await neis.getSchoolOne({
+      ATPT_OFCDC_SC_CODE: 'B10',
+      SD_SCHUL_CODE: '7091455',
+    })
+
+    const meal = await school.getMealOne({ MLSV_YMD: '20230331' })
+
+    const data = {
+      cal: meal.getCal(),
+      dish: meal.getDish(),
+      dishName: meal.getDishName(),
+      nutrition: meal.getNutrition(),
+      origin: meal.getOrigin(),
+    }
+
+    logger.info(data)
+
+    expect(data.cal).toBe(901.4)
+    expect(data.dish).toMatchObject<string[]>(data.dish)
+    expect(data.dishName).toMatchObject<string[]>(data.dishName)
+    expect(data.nutrition).toMatchObject<{ name: string; amount: number }[]>(
+      data.nutrition
+    )
+    expect(data.origin).toMatchObject<{ name: string; origin: string }[]>(
+      data.origin
+    )
+  })
 })
