554745f159666ca2f38194abc29cf1d1531acd49
Starcea <stardev.uwu@gmail.com>
2023-06-07 23:58:47 +0900

    feat: parsers with stdin

---
+++ scripts/parse_param.py
@@ -0,0 +1,44 @@
+from re import match, sub
+
+raw = ""
+while i := input():
+    raw += i
+    raw += "\n"
+
+items = [
+    sub(r" +", " ", " ".join(x).replace("\t", ""))
+    for x in (x.strip().split(" ") for x in raw.split("\n") if x.strip())
+]
+
+print(items)
+
+regex = r"^(.*) (.*)\((필수|선택)\) (.*)$"
+result = []
+
+
+def string_to_type(name: str) -> str:
+    if name == "STRING":
+        return "string"
+    elif name == "INTEGER":
+        return "number"
+    else:
+        raise NotImplementedError("not implemented")
+
+
+for data in items:
+    matches = match(regex, data)
+
+    if not matches:
+        print("not matched:", data)
+        continue
+
+    name, typeName, required, description = matches.groups()
+
+    result.extend(
+        [
+            f"/** {description} */",
+            f'readonly {name}{"" if required == "필수" else "?"}: {string_to_type(typeName)}',
+        ]
+    )
+
+print("\n".join(result))
---
+++ scripts/parse_response.py
@@ -0,0 +1,30 @@
+from re import match, sub
+
+raw = ""
+while i := input():
+    raw += i
+    raw += "\n"
+
+items = [
+    sub(r" +", " ", " ".join(x).replace("\t", ""))
+    for x in (x.strip().split(" ") for x in raw.split("\n") if x.strip())
+]
+
+print(items)
+
+regex = r"^(\d*) (.*) (.*)$"
+result = []
+
+for data in items:
+    matches = match(regex, data)
+
+    if not matches:
+        print("not matched:", data)
+        continue
+
+    name, description = matches.groups()[1:3]
+
+    result.append(f"/** {description} */")
+    result.append(f"readonly {name}: str")
+
+print("\n".join(result))
